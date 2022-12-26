e69b6baa93411567ef9f4ae4017cc665f4aee26e
cleverAkanimoh <crushclever1@gmail.com>
2022-12-26 11:35:05 +0100

    changed 6 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Info from './Components/Info';
+import About from './Components/About';
+import Interests from './Components/Interests';
+import Footer from './Components/Footer';
 
 const App = () => {
-  // const [count, setCount] = useState(0)
 
   return (
     <div>
       <Info />
+      <About />
+      <Interests />
+      <Footer />
     </div>
   )
 }
---
+++ src/Components/Interests.jsx
@@ -0,0 +1,7 @@
+const Interests = () => {
+    return ( 
+        <h2>Interest Component</h2>
+     );
+}
+ 
+export default Interests;
