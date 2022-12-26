3c65a2b90d435720ab02a499fd46348320652822
cleverAkanimoh <crushclever1@gmail.com>
2022-12-26 11:50:15 +0100

    modified Info.jsx in Components

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -1,6 +1,17 @@
+import image from '../assets/my-image.jpg';
+
 const Info = () => {
     return ( 
-        <h2>Info Component</h2>
+        <header>
+            <img src={image} alt="clever akanimoh" width="100"/>
+            <h3>Clever Akanimoh</h3>
+            <h5>FullStack Developer</h5>
+            <small>crushclever1@gmail.com</small>
+            <div className="link-div" >
+                <a href="" className="email-button">Email</a>
+                <a href="" className="linkedIn-button">LinkedIn</a>
+            </div>
+        </header>
      );
 }
  
